bc43e246b361da0bbe40c6e966daa7fa3af9f4c7
Lisa Heffernan <lisaheffernan@outlook.com>
2022-06-04 20:42:21 +0100

    kata 4 human cat and dog years code and test

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,9 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = number => {
+    const catMultiplier = 4;
+    const dogMultiplier = 5;
+    const formula = ((animal, number) => {return 15 + 9 + animal * (number - 2)});    
+    const array = [number, formula(catMultiplier, number), formula(dogMultiplier, number)];
+    return array
+};
 
 module.exports = humanCatDogYears;
