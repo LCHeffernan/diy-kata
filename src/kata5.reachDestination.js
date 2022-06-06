0ff2c6199d103f48c3def621e33f989f43c61eaf
Lisa Heffernan <lisaheffernan@outlook.com>
2022-06-06 19:25:28 +0100

    kata 5 reach destination test working

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,12 +1,11 @@
 
 const reachDestination = (distance, speed) => {
-// console.log('I should be there in ' + 4.5 + ' hours.')
 let time = (distance/speed).toFixed(1);
 const timeWholeHours = Math.trunc(time);
 const decimal = time - timeWholeHours;
 if(decimal <= 0.5){time = timeWholeHours + 0.5}
 else {time = timeWholeHours + 1};
-console.log('I should be there in ' + time + ' hours.')
+return 'I should be there in ' + time + ' hours.';
 };
-reachDestination(44, 10);
+
 module.exports = reachDestination;
