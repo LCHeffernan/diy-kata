36e5181ef3d3f76ae1327ceb2c9e9d760845c04f
Lisa Heffernan <lisaheffernan@outlook.com>
2022-06-06 19:29:25 +0100

    kata 7 get employer role test working

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -2,24 +2,8 @@ const getEmployerRole = (employeeName, employees) => {
 for (let i = 0; i<employees.length; i++)
 {
     if(employees[i].name === employeeName)
-    {console.log( employees[i].role)}
+    {return( employees[i].role)}
 }
 };
-
-const employees = [{
-    name: 'Satti',
-    role: 'Developer'
-  }, 
-  {
-    name: 'Jenny',
-    role: 'Sales Associate'
-  }, 
-  {
-    name: 'Javid',
-    role: 'Human Recommended Reading Assistant'
-  }];
-
-  getEmployerRole('Javid',employees);
-
 
 module.exports = getEmployerRole;
